7e0db43076acb988121562e6b1a0fc0786643648
ikramadam <126071398+ikramadam@users.noreply.github.com>
2023-05-09 11:41:54 +0200

    crear repositorio

---
+++ src/main/kotlin/com/example/tunejarbackend/TunejarBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tunejarbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TunejarBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<TunejarBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tunejarbackend/TunejarBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tunejarbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TunejarBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
