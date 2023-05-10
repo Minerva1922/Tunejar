2dd48be9428e7eef377ca72b04409f3dc9838964
Minerva1922 <minervapv83@gmail.com>
2023-05-10 12:19:04 +0200

    tenemos un header

---
+++ src/lib/header.svelte
@@ -0,0 +1,22 @@
+<script>
+    import logo from "../assets/tunejar-logo.png";
+</script>
+
+<header>
+   <img src= {logo} alt="logo de tunejar"> 
+   <div>login</div>
+
+</header>
+
+<style>
+    header{
+        height: 50px;
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+    }
+    img {
+        height: 100%;
+    }
+
+</style>
