3b6a51613d25557546e24097488fa9208aae5456
Minerva1922 <minervapv83@gmail.com>
2023-05-18 13:11:48 +0200

    añadido corazon

--- src/lib/TarjetaDeCancion.svelte
+++ src/lib/TarjetaDeCancion.svelte
@@ -1,6 +1,6 @@
 <script>
-    import {each} from "svelte/internal";
-
+    import { each } from "svelte/internal";
+    import Corazon from "./corazon.svelte";
     export let info;
 </script>
 
@@ -9,6 +9,7 @@
         <h3>{info.title}</h3>
         <p>{info.author} ({info.releaseYear})</p>
     </div>
+    <Corazon/>
 </div>
 
 <style>
@@ -18,43 +19,23 @@
         position: relative;
         width: 200px;
         height: 200px;
+        margin: 10%;
+        border-radius: 5px;
     }
-    
 
     .info {
-        bottom: 0;
         margin: 5%;
         display: flex;
-        position: absolute;
+
         text-align: center;
         flex-direction: column;
         color: black;
-        background-color: rgba(255, 255, 255, 0.3);
-
+        background-color: rgba(255, 255, 255, 0.669);
+        visibility: hidden;
     }
 
-
-    /*.info h3,*/
-    /*.info p {*/
-    /*    top: 50%;*/
-    /*    left: 50%;*/
-    /*    transform: translate(-50%, -50%);*/
-    /*    background-color: #642245;*/
-    /*    border: 2px solid #ff1493;*/
-    /*    box-shadow: 0 0 10px #ff1493, 0 0 20px #ff1493, 0 0 30px #ff1493,*/
-    /*    0 0 40px #ff1493;*/
-    /*    color: white;*/
-    /*    padding: 10px;*/
-    /*    border-radius: 20px;*/
-    /*    opacity: 0;*/
-    /*    transition: opacity 0.3s ease;*/
-    /*    text-align: center;*/
-
-    /*}*/
-
-    /*.info p:hover,*/
-    /*.info h3:hover {*/
-    /*    opacity: 1;*/
-    /*}*/
+    .card:hover .info{
+        visibility: visible;
+    }
 
 </style>
